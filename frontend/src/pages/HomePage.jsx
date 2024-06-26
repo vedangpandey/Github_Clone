c0d028f6f80b13f043fbbfa315bcbb3d51dfeb6b
Vedang <vedangssjgod@gmail.com>
2024-06-26 23:51:54 +0530

    github api integrated with search and sort

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -12,20 +12,25 @@ function HomePage() {
   const [loading,setLoading]=useState(false);
   const [sortType,setSortType]=useState("recent")
   const user=true;
-  const getUserFileAndRepos=useCallback(async ()=>{
+  const getUserFileAndRepos=useCallback(async (username="vedangpandey")=>{
     setLoading(true)
     try {
-      const userRes= await fetch('https://api.github.com/users/vedangpandey')
-      console.log(1)
+      const userRes= await fetch(`https://api.github.com/users/${username}`,{
+        headers:{
+          authorization:`token ${import.meta.env.VITE_GITHUB_API_KEY}`,
+        },
+      });
+      
       const userProfile= await userRes.json()
       
       setUserProfile(userProfile);
       const repoRes=await fetch(userProfile.repos_url)
       const repos=await repoRes.json()
-      console.log(repos)
+      console.log(userProfile);
+      console.log(repos);
       setRepos(repos)
+      return {userProfile,repos};
     } catch (error) {
-      console.log(error)
       toast.error(error.message)
     }finally{
       setLoading(false);
@@ -34,13 +39,37 @@ function HomePage() {
   useEffect(()=>{
     getUserFileAndRepos();
   },[getUserFileAndRepos])
+  const onSearch= async (e,username)=>{
+    e.preventDefault();
+    setLoading(true);
+    setRepos([])
+    setUserProfile(null);
+    const {userProfile,repos}=await getUserFileAndRepos(username);
+    setUserProfile(userProfile)
+    setRepos(repos)
+    setLoading(false);
+    setSortType('recent')
+  }
+  const onSort=(sortType)=>{
+    if(sortType === "recent"){
+      repos.sort((a,b)=>new Date(b.created_at)-new Date(a.created_at));
+    }else if(sortType === "stars"){
+      repos.sort((a,b)=>b.stargazers_count-a.stargazers_count);
+    }
+    else if(sortType === "forks"){
+      repos.sort((a,b)=>b.forks_count-a.forks_count)
+    }
+    setSortType(sortType);
+    setRepos([...repos]);
+  }
+
   return (
     <div className='m-4'>
-      <Search />
-      <SortRepos />
+      <Search onSearch={onSearch} />
+      {repos.length>0 && <SortRepos onSort={onSort} sortType={sortType} />}
       <div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
         {userProfile && !loading && < ProfileInfo userProfile={userProfile} />}
-        {repos.length>0 && <Repos repos={repos} />}
+        { <Repos repos={repos} />}
         {loading && <Spinner />}
       </div>
     </div>
