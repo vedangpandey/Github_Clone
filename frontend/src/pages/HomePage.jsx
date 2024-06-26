0266d41a5bf2a58e1cf24e695399b965c4a5ef47
Vedang <vedangssjgod@gmail.com>
2024-06-26 09:32:38 +0530

    HomePage was made

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Search from '../components/Search'
+import SortRepos from '../components/SortRepos'
+import ProfileInfo from '../components/ProfileInfo'
+import Repos from '../components/Repos'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className='m-4'>
+      <Search />
+      <SortRepos />
+      <div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+        <ProfileInfo/>
+        <Repos />
+      </div>
+    </div>
   )
 }
 
